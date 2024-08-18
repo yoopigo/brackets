c236008a336dc742c336612b92fcf176a78bf3e7
Andrey <collerggggggg@gmail.com>
2024-08-19 03:21:53 +0500

    Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const bracketsMap = {};
+
+  for (const [open, close] of bracketsConfig) {
+    bracketsMap[open] = close;
+  }
+  const stack = [];
+  for (const char of str) {
+    if (bracketsMap[char]) {
+      if (char === bracketsMap[char] && stack[stack.length - 1] === char) {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    } else {
+      const lastOpen = stack.pop();
+      if (bracketsMap[lastOpen] !== char) {
+        return false;
+      }
+    }
+  }
+  if (stack.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
